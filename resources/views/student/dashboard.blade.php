82a37a37db5de883892f4b7f990fb4010cca46f7
jashmavani6574@gmail.com <jashmavani6574@gmail.com>
2023-02-20 18:07:30 +0530

    show-data-on-student-dashboard commit

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -1,3 +1,41 @@
+@extends('layout/student-layout')
 
-<h1>Student Dashboard</h1>
-<a href="/logout">Logout</a>
+@section('space-work')
+
+<h2>Exam</h2>
+
+<table class="table">
+    <thead>
+        <th>#</th>
+        <th>Exam Name</th>
+        <th>Subject Name</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Total Attempt</th>
+        <th>Available Attempt</th>
+        <th>Copy Link</th>
+    </thead>
+
+    <tbody>
+        @if(count($exams) > 0)
+        @php $count = 1; @endphp
+            @foreach($exams as $exam)
+                <tr>
+                    <td>{{ $count++ }}</td>
+                    <td>{{ $exam->exam_name }}</td>
+                    <td>{{ $exam->subjects[0]['subject'] }}</td>
+                    <td>{{ $exam->date }}</td>
+                    <td>{{ $exam->time }} Hrs</td>
+                    <td>{{ $exam->attempt }} Time/s</td>
+                </tr>
+            @endforeach
+        @else
+
+        <tr>
+            <td colspan="8">There No Exams</td>
+        </tr>
+        @endif
+    </tbody>
+</table>
+
+@endsection
