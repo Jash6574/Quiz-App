44048585da4518baefbe0dcf6de24eaa0ddb2107
jashmavani6574@gmail.com <jashmavani6574@gmail.com>
2023-02-20 20:18:32 +0530

    dynamiclink commit

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -27,6 +27,8 @@
                     <td>{{ $exam->date }}</td>
                     <td>{{ $exam->time }} Hrs</td>
                     <td>{{ $exam->attempt }} Time/s</td>
+                    <td></td>
+                    <td><a href="#" class="copy" data-code="{{ $exam->enterance_id }}"><i class="fa fa-copy"></i></a></td>
                 </tr>
             @endforeach
         @else
@@ -37,5 +39,27 @@
         @endif
     </tbody>
 </table>
+
+
+<script>
+    $(document).ready(function(){
+        $('.copy').click(function(){
+            $(this).parent().append('<span class="copied_text">&nbsp;&nbsp;Copied!</span>');
+
+            var code = $(this).attr('data-code');
+            var url = "{{URL::to('/')}}/exam/"+code;
+
+            var $temp = $("<input>");
+            $("body").append($temp);
+            $temp.val(url).select();
+            document.execCommand("copy");
+            $temp.remove();
+
+            setTimeout(()=>{
+                $('.copied_text').remove();
+            },1000);
+        })
+    })
+</script>
 
 @endsection
