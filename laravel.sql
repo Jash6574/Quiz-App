ecd75fcbd4b1c891443abb67b0efe10e17978689
jashmavani6574@gmail.com <jashmavani6574@gmail.com>
2023-02-25 17:14:40 +0530

    database

--- laravel.sql
+++ laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2023 at 07:03 AM
+-- Generation Time: Feb 25, 2023 at 12:44 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -36,22 +36,6 @@ CREATE TABLE `answers` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `answers`
---
-
-INSERT INTO `answers` (`id`, `question_id`, `answer`, `is_correct`, `created_at`, `updated_at`) VALUES
-(1, 1, 'XA', 0, '2023-01-30 08:19:56', '2023-01-30 08:19:56'),
-(2, 1, 'DATA', 0, '2023-01-30 08:19:56', '2023-01-30 08:19:56'),
-(3, 1, 'BASE', 0, '2023-01-30 08:19:56', '2023-01-30 08:19:56'),
-(4, 1, 'DATA BASE MANAGMENT SYSTEM', 1, '2023-01-30 08:19:56', '2023-01-30 08:19:56'),
-(5, 2, 'name', 0, '2023-01-31 08:55:49', '2023-01-31 08:55:49'),
-(6, 2, 'name2', 1, '2023-01-31 08:55:49', '2023-01-31 08:55:49'),
-(18, 5, '19', 1, '2023-02-19 06:47:14', '2023-02-19 06:47:14'),
-(19, 5, '20', 0, '2023-02-19 06:47:14', '2023-02-19 06:47:14'),
-(20, 5, '21', 0, '2023-02-19 06:47:14', '2023-02-19 06:47:14'),
-(21, 5, '22', 0, '2023-02-19 06:47:14', '2023-02-19 06:47:14');
-
 -- --------------------------------------------------------
 
 --
@@ -71,13 +55,6 @@ CREATE TABLE `exams` (
   `date` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `exams`
---
-
-INSERT INTO `exams` (`id`, `exam_name`, `subject_id`, `attempt`, `marks`, `enterance_id`, `created_at`, `updated_at`, `time`, `date`) VALUES
-(10, 'maths-test', 2, 2, 32, 'EI\'d63f8a470dcb22', '2023-02-24 11:50:08', '2023-02-24 09:33:05', '00:02', '2023-02-24');
-
 -- --------------------------------------------------------
 
 --
@@ -92,16 +69,6 @@ CREATE TABLE `exams_answers` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `exams_answers`
---
-
-INSERT INTO `exams_answers` (`id`, `attempt_id`, `question_id`, `answer_id`, `created_at`, `updated_at`) VALUES
-(40, 20, 2, 5, '2023-02-23 18:41:37', '2023-02-23 18:41:37'),
-(41, 22, 1, 2, '2023-02-24 12:06:19', '2023-02-24 12:06:19'),
-(42, 22, 5, 19, '2023-02-24 12:06:19', '2023-02-24 12:06:19'),
-(43, 22, 2, 5, '2023-02-24 12:06:19', '2023-02-24 12:06:19');
 
 -- --------------------------------------------------------
 
@@ -118,14 +85,6 @@ CREATE TABLE `exams_attempt` (
   `updated_id` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `exams_attempt`
---
-
-INSERT INTO `exams_attempt` (`id`, `exam_id`, `user_id`, `status`, `marks`, `updated_id`, `updated_at`) VALUES
-(21, 10, 24, 0, NULL, '2023-02-24 11:54:14', '2023-02-24 11:54:14'),
-(22, 10, 24, 0, NULL, '2023-02-24 12:06:19', '2023-02-24 12:06:19');
 
 -- --------------------------------------------------------
 
@@ -230,26 +189,6 @@ CREATE TABLE `qna_exams` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `qna_exams`
---
-
-INSERT INTO `qna_exams` (`id`, `exam_id`, `question_id`, `created_at`, `updated_at`) VALUES
-(7, 2, 5, '2023-02-20 08:02:06', '2023-02-20 08:02:06'),
-(10, 5, 1, '2023-02-23 06:05:51', '2023-02-23 06:05:51'),
-(11, 6, 2, '2023-02-23 06:05:58', '2023-02-23 06:05:58'),
-(12, 7, 5, '2023-02-23 06:06:07', '2023-02-23 06:06:07'),
-(13, 6, 1, '2023-02-23 06:41:28', '2023-02-23 06:41:28'),
-(14, 6, 5, '2023-02-23 06:41:28', '2023-02-23 06:41:28'),
-(15, 8, 1, '2023-02-23 18:02:56', '2023-02-23 18:02:56'),
-(16, 8, 2, '2023-02-23 18:02:56', '2023-02-23 18:02:56'),
-(17, 8, 5, '2023-02-23 18:02:56', '2023-02-23 18:02:56'),
-(18, 9, 1, '2023-02-23 18:20:03', '2023-02-23 18:20:03'),
-(19, 9, 2, '2023-02-23 18:20:03', '2023-02-23 18:20:03'),
-(20, 10, 1, '2023-02-24 11:50:36', '2023-02-24 11:50:36'),
-(21, 10, 2, '2023-02-24 11:50:36', '2023-02-24 11:50:36'),
-(22, 10, 5, '2023-02-24 11:50:36', '2023-02-24 11:50:36');
-
 -- --------------------------------------------------------
 
 --
@@ -263,15 +202,6 @@ CREATE TABLE `questions` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `questions`
---
-
-INSERT INTO `questions` (`id`, `question`, `created_at`, `updated_at`) VALUES
-(1, 'Full form of DBMS', '2023-01-30 08:19:56', '2023-01-30 08:19:56'),
-(2, 'enter yoour name', '2023-01-31 08:55:49', '2023-01-31 08:55:49'),
-(5, 'date', '2023-02-19 06:47:14', '2023-02-19 06:47:14');
-
 -- --------------------------------------------------------
 
 --
@@ -284,14 +214,6 @@ CREATE TABLE `subjects` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `subjects`
---
-
-INSERT INTO `subjects` (`id`, `subject`, `created_at`, `updated_at`) VALUES
-(1, 'Dbms', '2023-01-30 08:16:04', '2023-01-30 02:46:19'),
-(2, 'math', '2023-01-31 08:52:51', '2023-01-31 03:23:03');
 
 -- --------------------------------------------------------
 
@@ -316,8 +238,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `is_admin`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(23, 'Admin', 'admin@gmail.com', NULL, 1, '$2y$10$HF/7teSN915SUGexsoKKg.g6rV6Zf63CBeMWS9KDYUxumpAfnVb8O', NULL, '2023-02-22 01:54:28', '2023-02-22 01:54:28'),
-(24, 'jash mavani', 'jashmavani6574@gmail.com', NULL, 0, '$2y$10$p87Ioyx1Qqe9rVls.Qqjl.2PbaV7QDVrWLvQDrLB3eqsWIEvwbj7O', NULL, NULL, NULL);
+(23, 'Admin', 'admin@gmail.com', NULL, 1, '$2y$10$HF/7teSN915SUGexsoKKg.g6rV6Zf63CBeMWS9KDYUxumpAfnVb8O', NULL, '2023-02-22 01:54:28', '2023-02-22 01:54:28');
 
 --
 -- Indexes for dumped tables
@@ -419,19 +340,19 @@ ALTER TABLE `answers`
 -- AUTO_INCREMENT for table `exams`
 --
 ALTER TABLE `exams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `exams_answers`
 --
 ALTER TABLE `exams_answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT for table `exams_attempt`
 --
 ALTER TABLE `exams_attempt`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -455,7 +376,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `qna_exams`
 --
 ALTER TABLE `qna_exams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `questions`
